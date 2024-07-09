c2d7b46aa89b95c1b34ace7f4d9ac75c70160670
Andy S Alic <>
2024-07-09 11:26:07 +0200

    Support Eucaim build and customization fix chaimeleon-eu/dataset-explorer#183, fix asalic/dataset-explorer#150

--- configure.js
+++ configure.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 let pjson = require('./package.json');
 
 if (process.argv.length !== 3) {
-  console.error("This script requires an argument, either dev (for development execution), prod-test (for production test), or prod (for production release)");
+  console.error("This script requires an argument, either dev (for development execution), prod-test (for production test), prod (for production release), or prod-eucaim (production eucaim cluster");
   process.exit(1);
 }
 
