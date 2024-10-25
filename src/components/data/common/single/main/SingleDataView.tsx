47e87de201c44a2fa56a67f652aac4259a41ee44
Andy S Alic (asalic) <asalic@users.noreply.github.com>
2024-10-25 09:00:00 +0200

    SingleDataView Each child in a list should have a unique "key" prop fix chaimeleon-eu/dataset-explorer#230, fix asalic/dataset-explorer#202

--- src/components/data/common/single/main/SingleDataView.tsx
+++ src/components/data/common/single/main/SingleDataView.tsx
@@ -164,7 +164,7 @@ function SingleDataView<T extends SingleData>(props: SingleDataViewProps<T>): JS
                   <Nav variant="pills" className="flex-column mb-5">
                     {
                       props.tabs.map(s => 
-                        <Nav.Item  key={`singledata-categories-${s.eventKey}`}>
+                        <Nav.Item key={`singledata-categories-${s.eventKey}`}>
                           <Nav.Link eventKey={s.eventKey}>{s.title}</Nav.Link>
                         </Nav.Item>
                       )
@@ -175,7 +175,7 @@ function SingleDataView<T extends SingleData>(props: SingleDataViewProps<T>): JS
                   <Tab.Content>
                     {
                       props.tabs.filter(s => s.eventKey === props.activeTab).map(s => 
-                        <Tab.Pane eventKey={s.eventKey}>
+                        <Tab.Pane key={`singledata-categories-tab-${s.eventKey}`} eventKey={s.eventKey}>
                             {s.view}
                         </Tab.Pane>
                       )
